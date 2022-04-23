4b946de8cd029617096dc7268fe910223a71d31a
Edgar Castaneda <67249405+Edgar-JC@users.noreply.github.com>
2022-04-23 13:06:34 -0600

    More changes

--- src/js/asideBar.js
+++ src/js/asideBar.js
@@ -10,6 +10,8 @@ const navigationBar = d.querySelector("#navigationBar");
 function activateHamburgerButton() {
     const hamburgerBtn = d.querySelector("#hamburgerBtn");
     const linesOfBtn = hamburgerBtn.querySelectorAll(".line")
+    
+    //listener to active mobile class
     hamburgerBtn.addEventListener("click",(e)=>{
         if(e.target.id === "hamburgerBtn"|| e.target.parentNode.id === "hamburgerBtn"){
             navigationBar.classList.toggle("active-mobile");
@@ -23,23 +25,9 @@ function activateHamburgerButton() {
 
 function openAsideBar() {
     const openAsideBtn = d.querySelector(".open-aside-btn");
-    const textButtons = d.querySelectorAll(".link-btn__text");
-    const menuButtons = d.querySelectorAll(".link-btn");
-    const logoAsideBar = d.querySelector(".aside-logo");
-    const toolTip = d.querySelectorAll(".tooltip");
-    const pageContent = d.querySelector(".page-content");
-
     openAsideBtn.addEventListener("click",e=>{
         if(e.target === openAsideBtn || e.target.parentNode.contains(openAsideBtn)){
             navigationBar.classList.toggle("active-desktop")
-
-
-            //openAsideBtn.classList.toggle("aside-active");
-            logoAsideBar.classList.toggle("aside-active");
-            textButtons.forEach(item =>{item.classList.toggle("aside-active")})
-            menuButtons.forEach(item =>{item.classList.toggle("aside-active")})
-            toolTip.forEach(item =>{item.classList.toggle("aside-active");})
-            pageContent.classList.toggle("aside-active")
         }
     })
 }
